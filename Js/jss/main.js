945501288dbaf5eac91d215e952833b60dadf180
ArtemiyAnisim <144233084+ArtemiyAnisim@users.noreply.github.com>
2024-01-29 13:34:14 +0300

    Add files via upload

---
+++ Js/jss/main.js
@@ -0,0 +1,117 @@
+// Переменные
+// Задача 1
+//var num = 3;
+//alert(num);
+
+// Задача 2
+//var a = 10;
+//var b = 2;
+//var sum = a + b;
+//var diff = a - b;
+//var product = a * b;
+//var quotient = a / b;
+
+//console.log("Сумма: " + sum);
+//console.log("Разность: " + diff);
+//console.log("Произведение: " + product);
+//console.log("Частное: " + quotient);
+
+// Задача 3
+//var c = 15;
+//var d = 2;
+//var result = c + d;
+//console.log("Результат сложения c и d: " + result);
+
+// Задача 4
+//var a1 = 10;
+//var b1 = 2;
+//var c1 = 5;
+//var sumABC = a1 + b1 + c1;
+//console.log("Сумма a, b и c: " + sumABC);
+
+// Задача 5
+//var a2 = 17;
+//var b2 = 10;
+//var c2 = a2 - b2;
+//var d2 = 7;
+//var result2 = c2 + d2;
+//console.log("Результат сложения c и d: " + result2);
+
+// Строки
+// Задача 1
+//var str = 'Привет, Мир!';
+//console.log(str);
+
+// Задача 2
+//var str1 = 'Привет, ';
+//var str2 = 'Мир!';
+//var greeting = str1 + str2;
+//console.log(greeting);
+
+// Задача 3
+//var name = 'Artemiy';
+//console.log('Привет, ' + name + '!');
+
+// Задача 4
+//var age = 16;
+//console.log('Мне ' + age + ' лет!');
+
+// Функция prompt
+
+// Задача 1
+//var userName = prompt('Введите ваше имя:');
+//alert('Ваше имя: ' + userName);
+
+// Задача 2
+//var userNumber = prompt('Введите число:');
+//var square = Math.pow(userNumber, 2);
+//alert('Квадрат числа ' + userNumber + ' равен: ' + square);
+
+// Обращение к символам строки
+
+// Задача 1
+//var str = 'abcde';
+//var charA = str[0];
+//var charC = str[2];
+//var charE = str[4];
+
+//console.log('Символ "a": ' + charA);
+//console.log('Символ "c": ' + charC);
+//console.log('Символ "e": ' + charE);
+
+// Задача 2
+//var num = '12345';
+//var digit1 = parseInt(num[0]);
+//var digit2 = parseInt(num[1]);
+//var digit3 = parseInt(num[2]);
+//var digit4 = parseInt(num[3]);
+//var digit5 = parseInt(num[4]);
+
+//var product = digit1 * digit2 * digit3 * digit4 * digit5;
+
+//console.log('Произведение цифр числа ' + num + ': ' + product);
+
+// Практика
+
+// Задача 1
+//var secondsInHour = 60 * 60;
+//var secondsInDay = secondsInHour * 24;
+//var secondsInMonth = secondsInDay * 30;
+
+//console.log('Секунд в часе: ' + secondsInHour);
+//console.log('Секунд в сутках: ' + secondsInDay);
+//console.log('Секунд в месяце: ' + secondsInMonth);
+
+// Задача 2
+//var now = new Date();
+//var hour = now.getHours();
+//var minute = now.getMinutes();
+//var second = now.getSeconds();
+
+//console.log('Текущее время: ' + hour + ':' + minute + ':' + second);
+
+// Задача 3
+//var number = 5; 
+//var square = Math.pow(number, 2);
+
+//console.log('Квадрат числа ' + number + ': ' + square);
